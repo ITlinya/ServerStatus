b78b4d556b3e94e33b9733c82edd6f8d05d0c302
ITlinya <75784281+ITlinya@users.noreply.github.com>
2024-09-18 14:31:38 +0800

    Update server_install_amd.sh

--- server_install_amd.sh
+++ server_install_amd.sh
@@ -3,17 +3,35 @@ set -ex
 WORKSPACE=/opt/ServerStatus
 mkdir -p ${WORKSPACE}
 cd ${WORKSPACE}
-# 下载, arm 机器替换 x86_64 为 aarch64
+
+# 判断操作系统并安装 unzip
+if [ -x "$(command -v yum)" ]; then
+    yum install -y unzip
+elif [ -x "$(command -v apt-get)" ]; then
+    apt-get update
+    apt-get install -y unzip
+else
+    echo "不支持的操作系统，请手动安装 unzip"
+    exit 1
+fi
+
+# x86_64 or aarch64
 OS_ARCH="x86_64"
 latest_version=$(curl -m 10 -sL "https://api.github.com/repos/zdz/ServerStatus-Rust/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 wget --no-check-certificate -qO "server-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/zdz/ServerStatus-Rust/releases/download/${latest_version}/server-${OS_ARCH}-unknown-linux-musl.zip"
+
+# 解压文件
 unzip -o "server-${OS_ARCH}-unknown-linux-musl.zip"
+
 # systemd service
 mv -v stat_server.service /etc/systemd/system/stat_server.service
 systemctl daemon-reload
-# 启动
+
+# 启动服务
 systemctl start stat_server
-# 状态查看
+
+# 查看服务状态
 systemctl status stat_server
-# 开机自启
+
+# 设置开机自启
 systemctl enable stat_server
