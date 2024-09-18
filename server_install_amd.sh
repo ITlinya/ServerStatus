2a5f9474dc088d9dfce7d3a6b13f36ccbe633489
ITlinya <75784281+ITlinya@users.noreply.github.com>
2024-09-18 14:37:47 +0800

    Update server_install_amd.sh
    整合脚本，适配AMD和ARM

--- server_install_amd.sh
+++ server_install_amd.sh
@@ -4,7 +4,7 @@ WORKSPACE=/opt/ServerStatus
 mkdir -p ${WORKSPACE}
 cd ${WORKSPACE}
 
-# 判断操作系统并安装 unzip
+# 安装 unzip
 if [ -x "$(command -v yum)" ]; then
     yum install -y unzip
 elif [ -x "$(command -v apt-get)" ]; then
@@ -15,9 +15,21 @@ else
     exit 1
 fi
 
-# x86_64 or aarch64
-OS_ARCH="x86_64"
+# 设置 OS_ARCH
+ARCH=$(uname -m)
+if [ "$ARCH" == "x86_64" ]; then
+    OS_ARCH="x86_64"
+elif [ "$ARCH" == "aarch64" ]; then
+    OS_ARCH="aarch64"
+else
+    echo "不支持的架构: $ARCH"
+    exit 1
+fi
+
+# 获取最新版本号
 latest_version=$(curl -m 10 -sL "https://api.github.com/repos/zdz/ServerStatus-Rust/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+
+# 下载对应架构的文件
 wget --no-check-certificate -qO "server-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/zdz/ServerStatus-Rust/releases/download/${latest_version}/server-${OS_ARCH}-unknown-linux-musl.zip"
 
 # 解压文件
